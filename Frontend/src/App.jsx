f0ed1b9a09d1f2ff2a57a8fd682f0afcfbed1b0f
neelabhra-de <deneelabhra@gmail.com>
2026-04-17 21:06:40 +0530

    feat: Add InterviewProvider to manage interview state in the application

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { RouterProvider } from 'react-router'
 import { router } from './app.routes.jsx'
 import { AuthProvider } from './features/auth/auth.context.jsx'
+import { InterviewProvider } from './features/interview/interview.context.jsx'
 
 const App = () => {
   return (
     <div>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <InterviewProvider>
+          <RouterProvider router={router} />
+        </InterviewProvider>
+        
       </AuthProvider>
     </div>
   )
