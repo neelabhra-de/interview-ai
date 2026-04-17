b942ab0fa534948c59f3760bedc5364aac2179cd
neelabhra-de <deneelabhra@gmail.com>
2026-04-17 17:41:59 +0530

    feat: Enhance interview report functionality

--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -3,7 +3,6 @@ const authMiddleware = require("../middlewares/auth.middleware")
 const interviewController = require("../controllers/interview.controller")
 const upload = require("../middlewares/file.middleware")
 
-
 const interviewRouter = express.Router()
 
 
@@ -16,18 +15,27 @@ const interviewRouter = express.Router()
 interviewRouter.post("/", authMiddleware.authUser, upload.single("resume"), interviewController.generateInterViewReportController)
 
 /**
- * @route GET /api/interview/:id
- * @description fetch a generated interview report by id.
+ * @route GET /api/interview/report/:interviewId
+ * @description get interview report by interviewId.
  * @access private
  */
-interviewRouter.get("/:id", authMiddleware.authUser, interviewController.getInterviewReportByIdController)
+interviewRouter.get("/report/:interviewId", authMiddleware.authUser, interviewController.getInterviewReportByIdController)
+
 
 /**
- * @route GET /api/interview/:id/resume
- * @description download extracted resume text for a generated interview report.
+ * @route GET /api/interview/
+ * @description get all interview reports of logged in user.
  * @access private
  */
-interviewRouter.get("/:id/resume", authMiddleware.authUser, interviewController.downloadResumeTextController)
+interviewRouter.get("/", authMiddleware.authUser, interviewController.getAllInterviewReportsController)
+
+
+/**
+ * @route GET /api/interview/resume/pdf
+ * @description generate resume pdf on the basis of user self description, resume content and job description.
+ * @access private
+ */
+interviewRouter.post("/resume/pdf/:interviewReportId", authMiddleware.authUser, interviewController.generateResumePdfController)
 
 
 
