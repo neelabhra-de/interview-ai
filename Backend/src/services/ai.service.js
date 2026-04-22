952dfce5eb065d0239ad76a2c209d2c22c31511f
neelabhra-de <deneelabhra@gmail.com>
2026-04-22 12:56:06 +0530

    fix: enhance puppeteer launch options for improved performance and stability

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -59,27 +59,36 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 async function generatePdfFromHtml(htmlContent) {
     const browser = await puppeteer.launch({
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        headless: true
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu"
+        ]
     });
 
-
     const page = await browser.newPage();
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
 
+    await page.setContent(htmlContent, {
+        waitUntil: "networkidle0"
+    });
     const pdfBuffer = await page.pdf({
-        format: "A4", margin: {
+        format: "A4",
+        margin: {
             top: "20mm",
             bottom: "20mm",
             left: "15mm",
             right: "15mm"
         }
-    })
+    });
 
-    await browser.close()
-
-    return pdfBuffer
+    await browser.close();
+    return pdfBuffer;
 }
+
+
+
 
 async function generateResumePdf({ resume, selfDescription, jobDescription }) {
     const resumePdfSchema = z.object({
